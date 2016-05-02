0c98356bd15a18ea6deb97e06142c51ebab248f7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-05-02 19:27:47 +0200

    Update de_DE, thanks to bigant

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Your photo will be submitted to the Piwigo demo, pending for validation (or reject).'] = 'Ihr Foto wird an Piwigo Demo übermittelt, zur Bestätigung (oder Ablehnung) ausstehend.';
+$lang['You can edit any photo either from the gallery or from the batch manager.'] = 'Sie können jedes Foto der Galerie oder mit dem Batch-Manager bearbeiten.';
+$lang['pending in demo'] = 'Bestätigung in Demo ausstehend';
+$lang['A nice demo will attract more users.'] = 'Eine aussagekräftige Demo-Seite zieht mehr Benutzer an.';
+$lang['Once duplicated in the demo, you give permission to the Piwigo demo for displaying your photo.'] = 'Sobald das Foto in Demo dupliziert ist, geben Sie Piwigo Demo die Berechtigung, um Ihr Foto darzustellen.';
+$lang['The idea is to have high quality and fresh content in the demo and give credits to Piwigo community for this contribution.'] = 'Die Idee ist, eine qualitativ hochstehende Demo-Galerie mit neuen Inhalten bereitzustellen mit Credits für die Piwigo-Gemeinschaft für diese Beiträge.';
+$lang['This link will provide visibility to your Piwigo from humans and search engines as well.'] = 'Dieser Link ist sichtbar für Personen sowie Suchmaschinen und zeigt auf Ihre Piwigo-Seite.';
+$lang['You\'ll be notified by mail when your photo is validated.'] = 'Sie werden per E-Mail benachrichtigt, wenn Ihr Foto akzeptiert wurde.';
+$lang['What is it?'] = 'Um was geht\'s?';
+$lang['As a contributor, you keep the ownership of your photos.'] = 'Als Contributor behalten Sie das Urheberrecht über Ihre Fotos.';
+$lang['At any time, you can remove your photo with a single and immediate action.'] = 'Sie können Ihr Foto jederzeit und mit einer einzigen und unmittelbaren Aktion entfernen.';
+$lang['Contribute to demo'] = 'Zu Demo beitragen';
+$lang['Find a nice photo in your Piwigo and edit it.'] = 'Ein schönes Foto in Ihrer Piwigo-Galerie finden und bearbeiten.';
+$lang['How to contribute?'] = 'Wie mitmachen?';
+$lang['In the demo, on the page of the photo, you will see a "contributed by" info with a link to the contributor website.'] = 'In der Demo, auf der Seit des Fotos, wird der Hinweis »beigetragen von« mit einem Link zur Contributor-Webseite angezeigt.';
+$lang['More users means more improvements to make Piwigo even more awesome!'] = 'Mehr Benutzer bedeutet mehr Verbesserungen, um Piwigo noch besser zu machen!';
+$lang['On the edition page, click on the "contribute to demo" link.'] = 'Auf der Bearbeitungsseite den Link »Zu Demo beitragen« klicken.';
+$lang['Photos in the Piwigo demo are coming from Piwigo users all over the world.'] = 'Fotos in Piwigo Demo stammen von Piwigo Nutzern weltweit.';
+$lang['Quite easy! Let\'s start!'] = 'Ganz einfach! Loslegen!';
+$lang['Read me'] = 'Hinweise';
+$lang['Remove from demo'] = 'Von Demo entfernen';
+$lang['See in demo'] = 'In Demo ansehen';
+$lang['The Piwigo project needs you to create the most appealing demo.'] = 'Das Piwigo-Projekt braucht dich, um eine ansprechende Demo-Galerie zu erstellen.';
+$lang['The administrators of the demo manage a list of pending photos.'] = 'Die Demo-Administratoren verwalten eine Liste der ausstehenden Fotos.';
+$lang['We hope you\'ll love the idea and decide to become a contributor yourself.'] = 'Wir hoffen, das dich diese Idee begeistert und du ebenfalls mitmachst.';
