20fdee6c5273c1acf5971565f9d52b620db6303f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-05-03 06:21:06 +0200

    Add nl_NL, thanks to Kees Hessels

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['A nice demo will attract more users.'] = 'Een fraaie demo zal meer potentiële gebruikers aantrekken.';
+$lang['As a contributor, you keep the ownership of your photos.'] = 'Als bijdrag(st)er aan de demo, behoudt u het eigendom van uw foto\'s.';
+$lang['At any time, you can remove your photo with a single and immediate action.'] = 'U kunt uw fotobijdrage aan de demo op elk moment met een enkele en onmiddellijke actie verwijderen.';
+$lang['Contribute to demo'] = 'Draag bij aan de demo';
+$lang['Find a nice photo in your Piwigo and edit it.'] = 'Zoek een mooie foto in uw Piwigo-galerie en wijzig deze.';
+$lang['How to contribute?'] = 'Hoe bij te dragen?';
+$lang['In the demo, on the page of the photo, you will see a "contributed by" info with a link to the contributor website.'] = 'In de demo, op de pagina van uw foto, ziet u een "bijgedragen door" info met een link uw website.';
+$lang['More users means more improvements to make Piwigo even more awesome!'] = 'Meer gebruikers betekent meer verbeteringen om Piwigo nog beter te maken!';
+$lang['On the edition page, click on the "contribute to demo" link.'] = 'Klik op de "bijdragen aan demo"-link op de wijzigingspagina.';
+$lang['Once duplicated in the demo, you give permission to the Piwigo demo for displaying your photo.'] = 'Op het moment dat uw foto wordt gekopieerd naar de demo, geeft u tevens toestemming aan de Piwigo demo om uw foto weer te geven.';
+$lang['Photos in the Piwigo demo are coming from Piwigo users all over the world.'] = 'De foto\'s in de Piwigo-demo komen van gebruikers van over de hele wereld.';
+$lang['Quite easy! Let\'s start!'] = 'Best makkelijk! Laten we beginnen!';
+$lang['Read me'] = 'Lees';
+$lang['Remove from demo'] = 'Verwijderen van foto uit de demo';
+$lang['See in demo'] = 'Bekijken van de foto in de demo';
+$lang['The Piwigo project needs you to create the most appealing demo.'] = 'Het Piwigo project heeft uw hulp nodig om de meest aansprekende demo te maken.';
+$lang['The administrators of the demo manage a list of pending photos.'] = 'De beheerders van de demo te beheren een lijst van nog te beoordelen foto\'s.';
+$lang['The idea is to have high quality and fresh content in the demo and give credits to Piwigo community for this contribution.'] = 'Het idee is om een demo van hoge kwaliteit te krijgen en op deze manier den Piwigo gemeenschap te bedanken voor deze bijdrage.';
+$lang['This link will provide visibility to your Piwigo from humans and search engines as well.'] = 'Deze link zal voor meer bezoekers zorgen naar uw Piwigo-galerie, zowel door mensen als door zoekmachines.';
+$lang['We hope you\'ll love the idea and decide to become a contributor yourself.'] = 'We hopen dat u dit initiatief sympatiek vindt en besluit om zelf ook een bijdrage te leveren.';
