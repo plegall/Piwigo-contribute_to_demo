d88d7fd28f26dedc0c5066c96b2425a3b9b4c125
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-05-05 11:48:29 +0200

    Add cs_CZ, thanks to gandycz

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['What is it?'] = 'Co je to?';
+$lang['You can edit any photo either from the gallery or from the batch manager.'] = 'Můžete upravit fotografii jak z galerie tak z dávkového zpracování.';
+$lang['You\'ll be notified by mail when your photo is validated.'] = 'Budete upozorněni emailem jakmile bude vaše fotografie schválena.';
+$lang['Your photo will be submitted to the Piwigo demo, pending for validation (or reject).'] = 'Vaše fotografie bude vložena do Piwigo ukázky, čekající na schválení (nebo zamítnutí).';
+$lang['pending in demo'] = 'čeká na ukázku';
+$lang['A nice demo will attract more users.'] = 'Krásná ukázka přitáhne více uživatelů.';
+$lang['As a contributor, you keep the ownership of your photos.'] = 'Jako přispěvatel si stále ponecháte vlastnictví vašich fotografií.';
+$lang['At any time, you can remove your photo with a single and immediate action.'] = 'Kdykoliv můžete vaši fotografii jednoduše odebrat.';
+$lang['Contribute to demo'] = 'Přispívat k ukázce';
+$lang['Find a nice photo in your Piwigo and edit it.'] = 'Najděte krásnou fotografii ve vašem Piwigo a upravte ji.';
+$lang['How to contribute?'] = 'Jak přispívat?';
+$lang['In the demo, on the page of the photo, you will see a "contributed by" info with a link to the contributor website.'] = 'V ukázce, na stránce fotografie, uvidíte informaci "přispěl" s odkazem na stránku přispěvatele. ';
+$lang['More users means more improvements to make Piwigo even more awesome!'] = 'Více uživatelů znamená více vylepšení a Piwigo bude úžasnější!';
+$lang['On the edition page, click on the "contribute to demo" link.'] = 'Na stránce editace, klikněte na "Přispívat k ukázce" odkaz.';
+$lang['Once duplicated in the demo, you give permission to the Piwigo demo for displaying your photo.'] = 'Jakmile je duplikát v ukázce, dáváte svolení Piwigo ukázce zobrazit vaší fotografii.';
+$lang['Photos in the Piwigo demo are coming from Piwigo users all over the world.'] = 'Fotografie v Piwigo ukázce pocházejí od Piwigo uživatelů z celého světa.';
+$lang['Quite easy! Let\'s start!'] = 'Celkem jednoduché! Tak začněme!';
+$lang['Read me'] = 'Přečti mě';
+$lang['Remove from demo'] = 'Odebrat z ukázky';
+$lang['See in demo'] = 'Zobrazit v ukázce';
+$lang['The Piwigo project needs you to create the most appealing demo.'] = 'Piwigo projekt vás potřebuje k vytvoření nejpřitažlivějšího ukázky.';
+$lang['The administrators of the demo manage a list of pending photos.'] = 'Administrátoři ukázky spravují seznam čekajících fotografií.';
+$lang['The idea is to have high quality and fresh content in the demo and give credits to Piwigo community for this contribution.'] = 'Hlavní myšlenkou je vysoká kvalita a aktuální obsah v ukázce a dát zásluhu Piwigo komunitě za tyto příspěvky.';
+$lang['This link will provide visibility to your Piwigo from humans and search engines as well.'] = 'Tento odkaz poskytne viditelnost vašeho Piwigo od uživatelů a vyhledávačů.';
+$lang['We hope you\'ll love the idea and decide to become a contributor yourself.'] = 'Doufáme že se vám bude tento nápad líbit a rozhodnete se stát přispěvatelem i vy.';
