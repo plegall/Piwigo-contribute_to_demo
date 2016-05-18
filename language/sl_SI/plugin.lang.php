3909549eab1aac1c631e23e06b5c4a02a28da95e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-05-18 05:14:18 +0200

    Add sl_SI, thanks to MarkoAm

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['A nice demo will attract more users.'] = 'Lepa predstavitev bo pritegnila več uporabnikov';
+$lang['As a contributor, you keep the ownership of your photos.'] = 'Kot darovalec obdržite lastništvo vaših fotografij.';
+$lang['At any time, you can remove your photo with a single and immediate action.'] = 'Kadarkoli lahko odstranite vaše fotografije z eno samim ukazom, ki se nemudoma izvede.';
+$lang['Contribute to demo'] = 'Prispevajte v predstavitev';
+$lang['Find a nice photo in your Piwigo and edit it.'] = 'Poiščite lepo fotografijo v vaši Piwigo in jo uredite';
+$lang['How to contribute?'] = 'Kako prispevati?';
