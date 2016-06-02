53267de95cba9938518c8369b1e1440652d6ba76
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-06-02 06:29:09 +0200

    Add zh_CN, thanks to dennisyan

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['The idea is to have high quality and fresh content in the demo and give credits to Piwigo community for this contribution.'] = '这个主意是为了使 demo 中拥有高质量的新鲜内容，并通过贡献对 Piwigo 社区给予信任。';
+$lang['This link will provide visibility to your Piwigo from humans and search engines as well.'] = '此链接将同时为人类和搜索引擎提供您的 Piwigo 的展示。';
+$lang['We hope you\'ll love the idea and decide to become a contributor yourself.'] = '我们希望您喜欢这个主意，并决定成为一位贡献者。';
+$lang['pending in demo'] = '等待审核';
+$lang['You\'ll be notified by mail when your photo is validated.'] = '您的图片通过审核后您将收到邮件通知。';
+$lang['Your photo will be submitted to the Piwigo demo, pending for validation (or reject).'] = '您的图片将被提交至 Piwigo demo 并等待通过审核（或被拒绝）。';
+$lang['Read me'] = '自述';
+$lang['Remove from demo'] = '从 demo 中移除';
+$lang['See in demo'] = '在 demo 中查看';
+$lang['The Piwigo project needs you to create the most appealing demo.'] = '为创造吸引人的 demo，Piwigo 项目需要您的贡献。';
+$lang['The administrators of the demo manage a list of pending photos.'] = 'demo 的管理员管理着待审核图片。';
+$lang['On the edition page, click on the "contribute to demo" link.'] = '在编辑页面上，点击 "贡献至 demo" 链接。';
+$lang['Once duplicated in the demo, you give permission to the Piwigo demo for displaying your photo.'] = '一旦复制到 demo，就意味着您授权 Piwigo demo 展示您的图片。';
+$lang['Photos in the Piwigo demo are coming from Piwigo users all over the world.'] = 'Piwigo demo 中的图片来自全世界的 Piwigo 用户。';
+$lang['Quite easy! Let\'s start!'] = '非常简单！让我们开始吧！';
+$lang['A nice demo will attract more users.'] = '一个好的demo演示会吸引更多的用户。';
+$lang['As a contributor, you keep the ownership of your photos.'] = '作为一位贡献者，您将保留您的图片的所有权。';
+$lang['At any time, you can remove your photo with a single and immediate action.'] = '任何时候您都可以简单迅速地移除您的图片。';
+$lang['Contribute to demo'] = '为 demo 作贡献';
+$lang['Find a nice photo in your Piwigo and edit it.'] = '在您的 Piwigo 中找一张好图片，并编辑它。';
+$lang['How to contribute?'] = '怎样作贡献？';
+$lang['In the demo, on the page of the photo, you will see a "contributed by" info with a link to the contributor website.'] = '在 demo 中，在图片页上，您将看到一条 "由 XX 贡献" 的信息，并链接到作出贡献的网站。';
+$lang['More users means more improvements to make Piwigo even more awesome!'] = '更多的用户意味着 Piwigo 将有更多的进步，变得更棒！';
+$lang['What is it?'] = '这是什么？';
+$lang['You can edit any photo either from the gallery or from the batch manager.'] = '任何图片您都可以在相册界面或批量管理界面上进行编辑。';
