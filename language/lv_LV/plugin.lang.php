551aa80d3d2088bee3b6ba4ee5eb5866d520f7da
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-06-08 10:41:05 +0200

    Add lv_LV, thanks to agrisans

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['pending in demo'] = 'gaidot demo';
+$lang['Your photo will be submitted to the Piwigo demo, pending for validation (or reject).'] = '
+Jūsu fotoattēls tiks iesniegts Piwigo demo, sagaidot apstiprināšanu (vai noraidījumu).';
+$lang['You\'ll be notified by mail when your photo is validated.'] = 'Jums tiks paziņots pa pastu, ka jūsu fotogrāfija ir apstiprināta.';
+$lang['You can edit any photo either from the gallery or from the batch manager.'] = 'Jūs varat rediģēt jebkuru fotogrāfiju vai nu galerijā vai no grupas pārvaldītāja.';
+$lang['What is it?'] = 'Kas tas ir?';
+$lang['We hope you\'ll love the idea and decide to become a contributor yourself.'] = 'Mēs ceram, ka jums patīk šī ideja un vabūt pats nolemsit kļūt par atbalstītāju.';
+$lang['This link will provide visibility to your Piwigo from humans and search engines as well.'] = '
+Šī saite nodrošinās Piwigo pieejamību cilvēkiem un meklētājprogrammām.';
+$lang['The idea is to have high quality and fresh content in the demo and give credits to Piwigo community for this contribution.'] = 'Ideja ir, lai demo būtu augstas kvalitātes un svaigs saturs un tas dotu Piwigo komūnai kredītu par šo ieguldījumu.';
+$lang['The administrators of the demo manage a list of pending photos.'] = 'Demo administratori pārvalda rindā esošo fotogrāfiju sarakstu.';
+$lang['The Piwigo project needs you to create the most appealing demo.'] = 'Piwigo projektm nepieciešams, lai jūs izveidotu vispievilcīgāko demo.';
+$lang['Remove from demo'] = 'Izņemt no demo';
+$lang['Quite easy! Let\'s start!'] = 'Diezgan viegli! Sākam!';
+$lang['Photos in the Piwigo demo are coming from Piwigo users all over the world.'] = '
+Piwigo demo fotogrāfijas ir Piwigo lietotāju foto visā pasaulē.';
+$lang['Once duplicated in the demo, you give permission to the Piwigo demo for displaying your photo.'] = 'Kad dublēta demo, jūs dodat atļauju Piwigo demo rādīt jūsu fotogrāfiju.';
+$lang['On the edition page, click on the "contribute to demo" link.'] = 'Izdevuma lapā, noklikšķiniet uz saites "veicināt demo".';
+$lang['More users means more improvements to make Piwigo even more awesome!'] = 'Vairāk lietotāju nozīmē vairāk uzlabojumu, kas padarītu Piwigo vēl pievilcīgāku!';
+$lang['In the demo, on the page of the photo, you will see a "contributed by" info with a link to the contributor website.'] = 'Uz demo, kas atrodas uz foto lapas, jūs redzēsiet "ieguldītāja" informāciju ar saiti uz tā mājas lapu.';
+$lang['How to contribute?'] = 'Kā veicināt?';
+$lang['Contribute to demo'] = 'Veicināt demo';
+$lang['Find a nice photo in your Piwigo and edit it.'] = 'Sameklē jauku fotogrāfiju savā Piwigo un rediģē to.';
+$lang['At any time, you can remove your photo with a single and immediate action.'] = 'Jebkurā laikā, jūs varat izņemt savu foto ar vienu un tūlītēju darbību.';
+$lang['As a contributor, you keep the ownership of your photos.'] = 'Kā atbalstītājs, jūs saglabājat īpašumtiesības uz savām fotogrāfijām.';
+$lang['See in demo'] = 'Skatīt demo';
+$lang['Read me'] = 'Lasi';
+$lang['A nice demo will attract more users.'] = 'Jauks demo piesaistīts vairāk lietotāju';
