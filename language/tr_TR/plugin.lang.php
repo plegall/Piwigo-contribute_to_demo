db90c8522a2f532c2a50476b1e1e8758a136363c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-08-12 21:48:00 +0200

    Add tr_TR, thanks to Nakre

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['You\'ll be notified by mail when your photo is validated.'] = 'Resminiz onaylandığı zaman eposta ile bilgilendirileceksiniz.';
+$lang['pending in demo'] = 'demoda beklemede';
+$lang['Contribute to demo'] = 'Demoya katkıda bulun';
+$lang['Remove from demo'] = 'Demodan sil';
+$lang['What is it?'] = 'Bu nedir?';
+$lang['How to contribute?'] = 'Nasıl katkı sağlayabilirsin';
+$lang['Quite easy! Let\'s start!'] = 'Çok kolay, hadi başla';
+$lang['Read me'] = 'Beni Oku';
+$lang['See in demo'] = 'Demoda gör';
