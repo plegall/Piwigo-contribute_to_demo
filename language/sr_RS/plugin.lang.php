ebb68aa15bc64e943be5227ede9d47a7413260a6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-08-21 06:45:59 +0200

    Add sr_RS, thanks to gpopac

---
+++ language/sr_RS/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['What is it?'] = 'Шта је ово?';
+$lang['You can edit any photo either from the gallery or from the batch manager.'] = 'Можете уредити било коју фотографију из галерије или менаџера операција.';
+$lang['You\'ll be notified by mail when your photo is validated.'] = 'Бићете обавештени е-поруком када ваша слика буде проверена.';
+$lang['Your photo will be submitted to the Piwigo demo, pending for validation (or reject).'] = 'Ваша слика ће бити послата на Пивиго демо страницу, где ће сачекати проверу.';
+$lang['pending in demo'] = 'чека на проверу за демо';
+$lang['A nice demo will attract more users.'] = 'Лепа демо страница ће привући више корисника.';
+$lang['As a contributor, you keep the ownership of your photos.'] = 'Кад пошаљете фотографије и даље задржавате власништво над њима.';
+$lang['At any time, you can remove your photo with a single and immediate action.'] = 'У било ком тренутку можете уклонити слику, једним потезом.';
+$lang['Contribute to demo'] = 'Допринесу демо програму';
+$lang['Find a nice photo in your Piwigo and edit it.'] = 'Нађите неку лепу слику из ваше Пивиго галерије и уредите је.';
+$lang['How to contribute?'] = 'Како да помогнете?';
+$lang['In the demo, on the page of the photo, you will see a "contributed by" info with a link to the contributor website.'] = 'Унутар демо странице, на делу са фотографијом ћете наћи податке под називом „допринос аутора“ са везом до његове веб странице.';
+$lang['More users means more improvements to make Piwigo even more awesome!'] = 'Више корисника доноси и додатна побољшања како би Пивиго постао још бољи!';
+$lang['On the edition page, click on the "contribute to demo" link.'] = 'На страници са издањем кликните на везу „допринеси демо програму“.';
+$lang['Once duplicated in the demo, you give permission to the Piwigo demo for displaying your photo.'] = 'Умножавањем слике унутар демо странице, дајете овлашћења Пивигу да приказује вашу фотографију.';
+$lang['Photos in the Piwigo demo are coming from Piwigo users all over the world.'] = 'Фотографије у Пивиго демо страници долазе од корисника широм света.';
+$lang['Quite easy! Let\'s start!'] = 'Једноставно! Хајде да почнемо!';
+$lang['Read me'] = 'Прочитајте ово';
+$lang['Remove from demo'] = 'Уклони са демо странице';
+$lang['See in demo'] = 'Погледај на демо страници';
+$lang['The Piwigo project needs you to create the most appealing demo.'] = 'Можете помоћи Пивиго пројекту тако што направите најутицајнији демо снимак.';
+$lang['The administrators of the demo manage a list of pending photos.'] = 'Администратори демо странице управљају листом пристиглих фотографија.';
+$lang['The idea is to have high quality and fresh content in the demo and give credits to Piwigo community for this contribution.'] = 'Желимо да увек имамо занимљив садржај на Пивиго демо страници и да промовишемо Пивиго заједницу која је допринела развоју програма.';
+$lang['This link will provide visibility to your Piwigo from humans and search engines as well.'] = 'Ова веза ће омогућити видљивост ваше Пивиго галерије осталима на мрежи.';
+$lang['We hope you\'ll love the idea and decide to become a contributor yourself.'] = 'Надамо се да вам се допала идеја и да ћете дати свој допринос.';
