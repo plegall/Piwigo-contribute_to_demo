56b99c12d9806a300770ee3ba554053d5c2d1f97
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-04-12 22:35:37 +0200

    Add el_GR, thanks to bas_alba

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Contribute to demo'] = 'Συμβολή στο demo';
+$lang['At any time, you can remove your photo with a single and immediate action.'] = 'Ανά πάσα στιγμή, μπορείτε να αφαιρέσετε τις φωτογραφίες σας με μια ενιαία και άμεση δράση.';
+$lang['As a contributor, you keep the ownership of your photos.'] = 'Ως συνεργάτης, θα διατηρήσετε την ιδιοκτησία των φωτογραφιών σας.';
+$lang['A nice demo will attract more users.'] = 'Ένα ωραίο demo θα προσελκύσει περισσότερους χρήστες.';
